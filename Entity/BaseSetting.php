2f4effc9644038056d8d4ed0074af85487fb7822
Christian Raue <christian.raue@gmail.com>
2015-04-22 13:00:33 +0200

    added a mapped superclass

---
+++ Entity/BaseSetting.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Craue\ConfigBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @author Christian Raue <christian.raue@gmail.com>
+ * @copyright 2011-2015 Christian Raue
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+abstract class BaseSetting {
+
+	/**
+	 * @var string
+	 * @Assert\NotBlank
+	 */
+	protected $name;
+
+	/**
+	 * @var string|null
+	 */
+	protected $value;
+
+	/**
+	 * @var string|null
+	 */
+	protected $section;
+
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function setValue($value) {
+		$this->value = $value;
+	}
+
+	public function getValue() {
+		return $this->value;
+	}
+
+	public function setSection($section) {
+		$this->section = $section;
+	}
+
+	public function getSection() {
+		return $this->section;
+	}
+
+}
